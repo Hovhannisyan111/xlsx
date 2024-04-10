8136a4feb618d2cf2b05cab4ff598af3ecc22c53
Hovhannisyan111 <ogannisian.2004.01@gmail.com>
2024-04-10 14:39:13 +0400

    Sorted

--- excel.py
+++ excel.py
@@ -5,32 +5,43 @@ def get_content(fname):
         return f.read()
 
 def chars_dict(cnt):
-    letters = {}
     numbers = {}
     symbols = {}
+    vowels = {}
+    consonants = {}
     for i in cnt:
         if i.isalpha():
-            letters[i] = letters.get(i, 0) + 1
+            if i.lower() in "aeoui":
+                vowels[i]  = vowels.get(i, 0) + 1
+            else:
+                consonants[i] = consonants.get(i, 0) + 1
         elif i.isdigit():
             numbers[i] = numbers.get(i, 0) + 1
         else:
             symbols[i] = symbols.get(i, 0) + 1
-    return letters, numbers, symbols
+    return numbers, symbols, vowels, consonants
 
 def create_workbook(output):
     workbook = xlsxwriter.Workbook(output)
     worksheet = workbook.add_worksheet()
     return workbook, worksheet
 
-def excel(workbook, worksheet, letters, numbers, symbols):
+def excel(workbook, worksheet, numbers, symbols, vowels, consonants):
     worksheet.write(0, 0, "Letters")
+    worksheet.write(1, 0, "Vowels")
     worksheet.write(0, 1, "Numbers")
     worksheet.write(0, 2, "Symbols")
 
-    row = 1
-    for l, count in sorted(letters.items(), key=lambda x: x[1], reverse=True):
-        worksheet.write(row, 0, l + ":" + str(count))
+    row = 2
+    for v, count in sorted(vowels.items(), key=lambda x: x[1], reverse=True):
+        worksheet.write(row, 0, v + ":" + str(count))
         row += 1
+        
+    worksheet.write(row, 0, "Consonants")
+    row += 1
+    for c, count in sorted(consonants.items(), key=lambda x: x[1], reverse=True):
+        worksheet.write(row, 0, c +":" + str(count))
+        row += 1  
     row = 1
     for n, count in sorted(numbers.items(), key=lambda x: x[1], reverse=True):
         worksheet.write(row, 1, n + ":" + str(count))
@@ -42,10 +53,11 @@ def excel(workbook, worksheet, letters, numbers, symbols):
 
 def main():
     cnt = get_content("db.txt")
-    letters, numbers, symbols = chars_dict(cnt)
+    numbers, symbols, vowels, consonants = chars_dict(cnt)
     workbook, worksheet = create_workbook('ex.xlsx')
-    excel(workbook, worksheet, letters, numbers, symbols)
+    excel(workbook, worksheet, numbers, symbols, vowels, consonants)
     workbook.close()
 
 if __name__ == "__main__":
     main()
+
